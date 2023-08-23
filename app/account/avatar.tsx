931fbc68c2c3bad805af9731d12b3d6e41eba543
mohansky <38560439+mohansky@users.noreply.github.com>
2023-08-23 23:09:02 +0530

    Update avatar.tsx

--- app/account/avatar.tsx
+++ app/account/avatar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Database } from "../database.types";
+// import { Database } from "../database.types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
